4671a87136a5e229f8823db3dd7294b6ca51fcac
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-13 18:26:27 +0100

    add ReviewCard

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,19 @@
+const ReviewCard = () => {
+    return (
+        <div className="card-review">
+            <div className="card-body-review">
+                <p>
+                    text
+                </p>
+                <strong>
+                    vote
+                </strong>
+                <address><i>
+                    by name
+                    </i></address>
+            </div>
+        </div>
+    )
+}
+
+export default ReviewCard
