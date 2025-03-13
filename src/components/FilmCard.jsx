71face9708b491816cbd23e7f4c7ad151a5f5443
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-13 18:26:11 +0100

    add FilmCard

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,6 +1,29 @@
+import {Link} from "react-router-dom"
+
+
 const FilmCard = () => {
     return (
         <>
+        <div className="card">
+            <img className="card-img"
+            src="http://localhost:3000/img/films/titatic.jpg"
+             alt="" />
+             <div className="card-body">
+                <h4 className="card-title">
+                    Titolo film 
+                </h4>
+                <address><i>
+                   director </i></address>
+
+                   <h6>
+                    genre
+                   </h6>
+                   <p>
+                    abstract
+                   </p>
+                   <Link to="films/2" className="btn">See more</Link>
+             </div>
+        </div>
         </>
     )
 }
