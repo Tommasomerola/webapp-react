d153a33b1435ff2e02149cd6071aaa104b969402
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-13 18:27:25 +0100

    add FilmPage

--- src/pages/FilmPage.jsx
+++ src/pages/FilmPage.jsx
@@ -1,6 +1,44 @@
+import { Link } from "react-router-dom"
+
+import ReviewCard from "../components/ReviewCard"
+
+
 const FilmPage = ()=> {
     return (
         <>
+        <header>
+            <div>
+                <img 
+                src="http://localhost:3000/img/films/titatic.jpg" 
+                alt="" />
+            
+                <div>
+                    <h1>Titolo film</h1>
+
+                    <h3>
+                    <i>
+                        directed by director
+                    </i>
+                    </h3>
+                    <p>
+                        abstract
+                    </p>
+                </div>
+            </div>
+        </header>
+        <section id="reviews">
+            <div>
+                <h4>our community reviews</h4>
+            </div>
+            <ReviewCard/>
+            <ReviewCard/>
+            <ReviewCard/>
+            </section>
+            <footer>
+                <Link className="btn" to="/">Back to Home</Link>
+            </footer>
+            
+       
         </>
     )
 }
