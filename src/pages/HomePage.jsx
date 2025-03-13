c24dfc9f24450cdd911d679f13462b95f815ab28
TOMMASOMEROLA <MEROLATOMMASO77@GMAIL.COM>
2025-03-13 18:26:40 +0100

    add Home Page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,18 @@
+import FilmCard from "../components/FilmCard"
+
 const HomePage = () => {
     return (
         <>
-        
+            <h1>Films</h1>
+            <h2><i>Community nerd di film</i></h2>
+            <div className="container-homepage">
+                <FilmCard/>
+                <FilmCard/>
+                <FilmCard/>
+                <FilmCard/>
+                <FilmCard/>
+                <FilmCard/>
+            </div>
         </>
     )
 }
